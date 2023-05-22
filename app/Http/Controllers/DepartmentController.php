9350af4e975ed8166730cd314af987de1a7af8d3
fah <naphatsaran.fah@gmail.com>
2023-05-22 10:48:31 +0700

    update error message form department

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -12,8 +12,11 @@ class DepartmentController extends Controller
 
     public function store(Request $request) {
         $request->validate([
-            'department_name'=>'required|unique:departments|max:255'
-        ]);
+            'department_name'=>'required|unique:departments|max:10'
+        ],
+        ['department_name.required'=>"กรุณาป้อนชื่อแผนกด้วยค่ะ",
+        'department_name.max'=>"ห้ามป้อนเกิน 10 ตัวอักษร",
+    ]);
     }
 
 }
