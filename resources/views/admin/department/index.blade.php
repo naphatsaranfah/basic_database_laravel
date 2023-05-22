0acd97ae95b2077f36b2f46b201f888bb3213f32
fah <naphatsaran.fah@gmail.com>
2023-05-22 10:21:22 +0700

    create form department

--- resources/views/admin/department/index.blade.php
+++ resources/views/admin/department/index.blade.php
@@ -9,9 +9,23 @@
     </x-slot>
 
     <div class="py-12 px-40">
-        <div class="container">
-            Hello Department
+        <div class="grid grid-cols-2 gap-4">
+            <div class="mb-5 font-bold"">ตารางข้อมูลแผนก</div>
+            <div class="">
+                <p class="mb-5 font-bold">แบบฟอร์ม</p>
+
+                <form action="" method="post">
+                    <div>
+                        <label for="department_name" class="">ชื่อตำแหน่งงาน</label>
+                    </div>
+                    <div>
+                        <input type="text" class="form-control mt-5" name="department_name">
+                    </div>
+                    <div>
+                        <input type="submit" value="บันทึก" class="mt-5 p-3 w-48 bg-blue-800 text-white rounded-md">
+                    </div>
+                </form>
+            </div>
         </div>
-    </div>
     </div>
 </x-app-layout>
